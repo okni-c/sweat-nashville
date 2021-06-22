1729707851ba03a879b9154fe126efda3b4cebbf
okni-c <oknistay@gmail.com>
2021-06-22 03:51:35 -0500

    added title helper, favicon

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.engine('handlebars', exphbs());
 app.use(express.static('public'))
 app.get('/', (req, res) => {
     //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-    res.render('main', { layout: 'index' });
+    res.render('main', { layout: 'index', title: 'Sweat Nashville - Home'});
 });
 
 app.get('/social', (req, res) => {
     //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-    res.render('social', { layout: 'index' });
+    res.render('social', { layout: 'index', title: 'Sweat Nashville - Social'});
 });
 
 app.listen(process.env.PORT || 3001, 

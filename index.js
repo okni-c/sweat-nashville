aa9a19950bcb9ab5500def51954e766c3bc2b9d2
okni-c <oknistay@gmail.com>
2021-06-22 04:23:01 -0500

    removed comments

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
 
-//Sets our app to use the handlebars engine
 app.set('view engine', 'handlebars');
-//Sets handlebars configurations (we will go through them later on)
+
 app.engine('handlebars', exphbs());
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
-    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
     res.render('main', { layout: 'index', title: 'Sweat Nashville - Home'});
 });
 
 app.get('/social', (req, res) => {
-    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
     res.render('social', { layout: 'index', title: 'Sweat Nashville - Social'});
 });
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,4 @@
 /**
-* Template Name: Day - v4.3.0
-* Template URL: https://bootstrapmade.com/day-multipurpose-html-template-for-free/
-* Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
 (function() {

166efc05db5fb73e283c8b832a93c11c8a28bf95
okni-c <oknistay@gmail.com>
2021-06-22 03:37:41 -0500

    adjusted hbs dir

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
 
-//Loads the handlebars module
-const handlebars = require('express-handlebars');
 //Sets our app to use the handlebars engine
 app.set('view engine', 'handlebars');
 //Sets handlebars configurations (we will go through them later on)
-app.engine('handlebars', handlebars({
-    layoutsDir: __dirname + '/views',
-}));
+app.engine('handlebars', exphbs());
+
 app.use(express.static('public'))
 app.get('/', (req, res) => {
     //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
@@ -17,7 +15,7 @@ app.get('/', (req, res) => {
 
 app.get('/social', (req, res) => {
     //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-    res.render('main', { layout: 'social' });
+    res.render('social', { layout: 'index' });
 });
 
 app.listen(process.env.PORT || 3001, 
